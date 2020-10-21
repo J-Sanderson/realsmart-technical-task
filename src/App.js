6c6107812d5ed8349f9e88e13f91715d629fb30a
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-21 12:33:21 +0100

    feat: set up basic NumberList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import NumberList from './components/NumberList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>realsmart Technical Task</h1>
+        <NumberList title="Unused numbers" />
+        <NumberList title="Used numbers" />
+      </div>
+    );
+  }
 }
 
 export default App;
