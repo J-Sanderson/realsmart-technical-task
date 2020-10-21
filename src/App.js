454dc42534ea083d515236d4fbf2461752bb7c85
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-21 12:46:49 +0100

    feat: generate and display numbers

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import './App.css';
 import NumberList from './components/NumberList';
 
 class App extends Component {
+
+  state = {
+    unused: [],
+    used: [],
+  };
+
+  componentDidMount = () => {
+    let numbers = [];
+    for (let i = 1; i <=100; i++) {
+      numbers.push(i);
+    }
+    this.setState({
+      unused: numbers,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>realsmart Technical Task</h1>
-        <NumberList title="Unused numbers" />
-        <NumberList title="Used numbers" />
+        <div className="container">
+          <NumberList title="Unused numbers" numbers={this.state.unused} />
+          <NumberList title="Used numbers" numbers={this.state.used} />
+        </div>
       </div>
     );
   }
