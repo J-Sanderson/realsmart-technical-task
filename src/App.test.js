6807d171cf12aca49b785f15d46d8d6d0d460cd4
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-24 12:03:09 +0100

    feat: test number swapping

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
 test('renders panels and buttons', () => {
@@ -8,10 +8,33 @@ test('renders panels and buttons', () => {
   const unused = getByTestId('unused');
   const used = getByTestId('used');
   const addUsed = getByText(/Add to used/i);
-  const addUnused = getByText(/Add to used/i);
+  const addUnused = getByText(/Add to unused/i);
 
   expect(unused).toBeInTheDocument();
   expect(used).toBeInTheDocument();
   expect(addUsed).toBeInTheDocument();
   expect(addUnused).toBeInTheDocument();
+});
+
+test('swaps numbers', () => {
+  const { getByTestId, getByText, getByLabelText } = render(<App />);
+  const unused = getByTestId('unused');
+  const addUsed = getByText(/Add to used/i);
+  const addUnused = getByText(/Add to unused/i);
+  const blank = getByText(/There are no numbers/);
+  const testNum = '100';
+
+  expect(unused).toHaveTextContent(testNum);
+  expect(blank).toBeInTheDocument();
+
+  fireEvent.click(getByLabelText(testNum));
+  fireEvent.click(addUsed);
+
+  expect(unused).not.toHaveTextContent(testNum);
+  expect(blank).not.toBeInTheDocument();
+
+  fireEvent.click(getByLabelText(testNum));
+  fireEvent.click(addUnused);
+
+  expect(unused).toHaveTextContent(testNum);
 });
